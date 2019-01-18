42417b0e2c40ae2b2af0e7d4a528a5c998c524c9
ge.tang@aispeech.com <ge.tang@aispeech.com>
2019-01-18 14:31:48 +0800

    111

--- server/main.js
+++ server/main.js
@@ -1,24 +1,32 @@
 const commands = require("./commands")
 const git = require("./lib/git")
+// const git = require("simple-git")()
 
-commands.login().then(async () => {
-  await git.init()
-  const gitclient = git.getInstance()
-  // await gitclient.pull('origin', 'master')
-  // job()
-  gitclient.add('./*').commit('Initial Commit').push('origin', 'master')
-})
+require('simple-git')()
+     .init()
+     .add('./*')
+     .commit('111')
+     .addRemote('origin', 'git@github.com:ge.tang@aispeech.com/aispeechfe.github.io.git')
+     .push('origin', 'master');
+
+// commands.login().then(async () => {
+//   // await git.init()
+//   // const gitclient = git.getInstance()
+//   // // await gitclient.pull('origin', 'master')
+//   // // job()
+//   // gitclient.add('./*').commit('Initial Commit').push('origin', 'master')
+// })
 
 
 
-async function job() {
-  const check = await commands.checkbuild()
-  console.log(check)
-  if (check.build) {
-    const detail = await commands.getCommitDetail(check.sha)
-    // console.log(detail)
-    await gitclient.pull('origin', 'master')
-    commands.filebuild(detail.files)
-    git.add('./*').commit('Initial Commit').push('origin', 'master')
-  }
-}
+// async function job() {
+//   const check = await commands.checkbuild()
+//   console.log(check)
+//   if (check.build) {
+//     const detail = await commands.getCommitDetail(check.sha)
+//     // console.log(detail)
+//     await gitclient.pull('origin', 'master')
+//     commands.filebuild(detail.files)
+//     git.add('./*').commit('Initial Commit').push('origin', 'master')
+//   }
+// }
