7d0df962a6db4c7d70d1058b61a690f266b20b3c
ge.tang@aispeech.com <ge.tang@aispeech.com>
2019-01-18 14:14:57 +0800

    Initial Commit

--- server/lib/git.js
+++ server/lib/git.js
@@ -7,7 +7,8 @@ module.exports = {
     const credentials = await inquirer.askGitPwd()
     git.addConfig('user.name', githubconfig.userName)
     .addConfig('user.email', githubconfig.userName)
-    // .addRemote('origin', `https://${githubconfig.userName}:${credentials.password}@github.com/${githubconfig.userName}/${githubconfig.repo}`)
+    .removeRemote('origin')
+    .addRemote('origin', `https://${githubconfig.userName}:${credentials.password}@github.com/${githubconfig.userName}/${githubconfig.repo}`)
   },
   getInstance: () => {
     return git
