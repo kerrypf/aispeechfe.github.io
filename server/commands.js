8180dce7f5b40137f604a621b17c58e41d1934cf
Your Name <you@example.com>
2019-01-19 16:56:29 +0800

    Initial Commit

--- server/commands.js
+++ server/commands.js
@@ -74,6 +74,7 @@ module.exports = {
       let list = file.readdirSync(`./files/${m}`)
       let html = template.layoutSubject(list, addrs)
       let path = `${m == 'index' ? './':'./subject/'}${m}.html`
+      console.log(path)
       file.writeFileSync(path, html)
     })
   }
