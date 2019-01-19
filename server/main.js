3921788772583a1422c2336bdf3419627220b446
Your Name <you@example.com>
2019-01-19 16:39:46 +0800

    Initial Commit

--- server/main.js
+++ server/main.js
@@ -1,32 +1,22 @@
 const commands = require("./commands")
-const git = require("./lib/git")
-// const git = require("simple-git")()
+const git = require("simple-git")()
 
-require('simple-git')()//.pull('origin', 'master')
-     // .init()
-     .add('./*')
-     .commit('111')
-    //  .addRemote('origin', 'git@github.com:ge.tang@aispeech.com/aispeechfe.github.io.git')
-     .push('origin', 'master');
-
-// commands.login().then(async () => {
-//   // await git.init()
-//   // const gitclient = git.getInstance()
-//   // // await gitclient.pull('origin', 'master')
-//   // // job()
-//   // gitclient.add('./*').commit('Initial Commit').push('origin', 'master')
-// })
+commands.login().then(async () => {
+  await git.pull('origin', 'master')
+  job()
+  git.add('./*').commit('Initial Commit').push('origin', 'master')
+})
 
 
 
-// async function job() {
-//   const check = await commands.checkbuild()
-//   console.log(check)
-//   if (check.build) {
-//     const detail = await commands.getCommitDetail(check.sha)
-//     // console.log(detail)
-//     await gitclient.pull('origin', 'master')
-//     commands.filebuild(detail.files)
-//     git.add('./*').commit('Initial Commit').push('origin', 'master')
-//   }
-// }
+async function job() {
+  const check = await commands.checkbuild()
+  console.log(check)
+  if (check.build) {
+    const detail = await commands.getCommitDetail(check.sha)
+    // console.log(detail)
+    await gitclient.pull('origin', 'master')
+    commands.filebuild(detail.files)
+    git.add('./*').commit('Initial Commit').push('origin', 'master')
+  }
+}
